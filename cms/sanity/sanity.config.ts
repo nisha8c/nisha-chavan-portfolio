93c9b31d17d7fb24b3870c9a294069f0e4d51722
Nisha Chavan <nisha.chavan@lekab.com>
2025-09-21 18:55:51 +0200

    CHANGES

--- cms/sanity/sanity.config.ts
+++ cms/sanity/sanity.config.ts
@@ -7,8 +7,8 @@ export default defineConfig({
   name: 'default',
   title: 'nisha-chavan-portfolio',
 
-  projectId: '0qq8l5wg',
-  dataset: 'production',
+  projectId: process.env.SANITY_PROJECT_ID!,
+  dataset: process.env.SANITY_DATASET!,
 
   plugins: [structureTool(), visionTool()],
 
