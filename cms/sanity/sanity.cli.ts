231cb53315d5b2f063ac0a91033e514ebe989985
Nisha Chavan <nisha.chavan@lekab.com>
2025-09-22 08:30:16 +0200

    CHANGES

--- cms/sanity/sanity.cli.ts
+++ cms/sanity/sanity.cli.ts
@@ -2,8 +2,8 @@ import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || '0qq8l5wg',
+    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production'
   },
   deployment: {
     /**
